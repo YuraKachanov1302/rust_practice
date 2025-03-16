edb43f372bba9adb0e4264f8d6f91b51ae2d3685
Юра Качанов 044 <135356539+YuraKachanov1302@users.noreply.github.com>
2025-03-16 16:03:29 +0200

    Update hw04.rs

---
+++ src/homework/hw04.rs
@@ -0,0 +1,8 @@
+def print_rhombus(n):
+    for i in range(n):
+        print(" " * (n - i - 1) + "*" * (2 * i + 1))
+    for i in range(n - 2, -1, -1):
+        print(" " * (n - i - 1) + "*" * (2 * i + 1))
+
+# Вызываем функцию с нужным размером
+print_rhombus(6)
