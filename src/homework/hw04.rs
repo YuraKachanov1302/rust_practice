de351e1fcc677318b4b234fa9f4186b76c4e34c1
Юра Качанов 044 <135356539+YuraKachanov1302@users.noreply.github.com>
2025-03-16 16:04:31 +0200

    Update hw04.rs

--- src/homework/hw04.rs
+++ src/homework/hw04.rs
@@ -1,8 +1,15 @@
-def print_rhombus(n):
-    for i in range(n):
-        print(" " * (n - i - 1) + "*" * (2 * i + 1))
-    for i in range(n - 2, -1, -1):
-        print(" " * (n - i - 1) + "*" * (2 * i + 1))
+const WIDTH: usize = 6; // Половина висоти ромба
 
-# Вызываем функцию с нужным размером
-print_rhombus(6)
+fn main() {
+    let mut output = String::new();
+
+    for i in 0..WIDTH {
+        output += &format!("{:width$}{}\n", "", "*".repeat(2 * i + 1), width = WIDTH - i);
+    }
+
+    for i in (0..WIDTH - 1).rev() {
+        output += &format!("{:width$}{}\n", "", "*".repeat(2 * i + 1), width = WIDTH - i);
+    }
+
+    print!("{}", output);
+}
